7892ad142d1791c953457f19dc6a02420647c5d2
qian-o <1324771795@qq.com>
2025-10-23 09:16:15 +0800

    Refactor platform-specific window logic
    Moved platform-specific window manipulation logic from `WindowEx.cs` to separate files `Win32.cs` and `X11.cs` for better organization and maintainability.
    
    - Removed Windows and Linux-specific methods from `WindowEx.cs` and replaced them with calls to `Win32.Penetration(handle)` and `X11.Penetration(handle)`.
    - Introduced `Win32.cs` for Windows-specific window manipulation using P/Invoke.
    - Added `X11.cs` for Linux-specific window manipulation using X11 libraries.
    - Improved code clarity and modularity by isolating platform-specific code.

---
+++ AvaloniaPenetration/Win32.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace AvaloniaPenetration;
+
+public static partial class Win32
+{
+    [LibraryImport("USER32.dll")]
+    private static partial int GetWindowLongPtrA(nint hWnd, int nIndex);
+
+    [LibraryImport("USER32.dll")]
+    private static partial int SetWindowLongPtrA(nint hWnd, int nIndex, int dwNewLong);
+
+    [LibraryImport("USER32.dll")]
+    private static partial int SetLayeredWindowAttributes(nint hwnd, uint crKey, byte bAlpha, uint dwFlags);
+
+    public static void Penetration(nint handle)
+    {
+        const int GWL_EXSTYLE = -20;
+        const int WS_EX_LAYERED = 0x00080000;
+        const int WS_EX_TRANSPARENT = 0x00000020;
+        const int LWA_ALPHA = 0x00000002;
+
+        if (SetWindowLongPtrA(handle, GWL_EXSTYLE, GetWindowLongPtrA(handle, GWL_EXSTYLE) | WS_EX_LAYERED | WS_EX_TRANSPARENT) is 0)
+        {
+            throw new Exception("SetWindowLongPtrA failed.");
+        }
+
+        if (SetLayeredWindowAttributes(handle, 0, 255, LWA_ALPHA) is 0)
+        {
+            throw new Exception("SetLayeredWindowAttributes failed.");
+        }
+    }
+}
---
+++ AvaloniaPenetration/X11.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.InteropServices;
+
+namespace AvaloniaPenetration;
+
+public static partial class X11
+{
+    [LibraryImport("libX11.so.6")]
+    private static partial nint XOpenDisplay(nint displayName);
+
+    [LibraryImport("libX11.so.6")]
+    private static partial void XCloseDisplay(nint display);
+
+    [LibraryImport("libX11.so.6")]
+    private static partial nint XCreateRegion();
+
+    [LibraryImport("libXext.so.6")]
+    private static partial void XShapeCombineRegion(nint display, nint window, int destKind, int xOff, int yOff, nint region, int op);
+
+    public static void Penetration(nint handle)
+    {
+        nint display = XOpenDisplay(0);
+
+        XShapeCombineRegion(display, handle, 2, 0, 0, XCreateRegion(), 0);
+
+        XCloseDisplay(display);
+    }
+}
